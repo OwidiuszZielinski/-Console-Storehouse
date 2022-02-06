d84d1272a7267db048ff727047146ce2ae607189
Owidiusz Zieliński <owi19955@gmail.com>
2022-02-06 12:49:12 +0100

    add field

--- src/pl/owi/ProductPC.java
+++ src/pl/owi/ProductPC.java
@@ -1,4 +1,32 @@
 package pl.owi;
+import java.util.ArrayList;
 
 public class ProductPC {
+
+    public String nettoprice;
+
+    public String itemname;
+
+    private String[] group = {"food","electronics","toys"};
+    String[] getGroup(){
+        return this.group;
+    }
+
+    //jesli nie zadziała sprobuje group i vat zrobic na Arrayliscie
+
+    private Integer[] vat = {23,8,5,0};
+    Integer[] getVat(){
+        return this.vat;
+    }
+
+    ProductPC(String itemname){
+        this.itemname = itemname;
+
+
+    }
+
+
+
+
+
 }
